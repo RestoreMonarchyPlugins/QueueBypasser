beccc53c4f3ccd26508760d010c1045b5c4347a3
RestoreMonarchy <35507241+RestoreMonarchy@users.noreply.github.com>
2021-01-13 18:52:19 +0100

    Add project files.

---
+++ QueueBypasser/QueueBypasserPlugin.cs
@@ -0,0 +1,41 @@
+﻿using Rocket.API;
+using Rocket.Core;
+using Rocket.Core.Logging;
+using Rocket.Core.Plugins;
+using SDG.Unturned;
+using Steamworks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestoreMonarchy.QueueBypasser
+{
+    public class QueueBypasserPlugin : RocketPlugin
+    {
+        protected override void Load()
+        {
+            Logger.Log($"{Name} {Assembly.GetName().Version} has been loaded!", ConsoleColor.Yellow);
+        }
+
+        protected override void Unload()    
+        {
+            Logger.Log($"{Name} has been unloaded!", ConsoleColor.Yellow);
+        }
+
+        void FixedUpdate()
+        {
+            foreach (var pending in Provider.pending.ToList())
+            {
+                if (R.Permissions.HasPermission(new RocketPlayer(pending.playerID.steamID.ToString()), "queuebypasser"))
+                {
+                    if (!pending.hasSentVerifyPacket)
+                    {
+                        pending.sendVerifyPacket();
+                    } 
+                }
+            }
+        }
+    }
+}
