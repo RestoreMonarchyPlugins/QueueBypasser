fd4d33228152a7cff815db3f345c6fa576e4ae1f
MCrow <michalwrona@hotmail.com>
2024-12-13 23:48:51 +0100

    Fix error when loading knownPlayers, but Players directory did not exist

--- QueueBypasser/Utils/KnownPlayers.cs
+++ QueueBypasser/Utils/KnownPlayers.cs
@@ -1,6 +1,7 @@
 ﻿using Rocket.Core.Logging;
 using SDG.Unturned;
 using Steamworks;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -9,40 +10,64 @@ namespace RestoreMonarchy.QueueBypasser.Utils
 {
     internal class KnownPlayers
     {
-        private HashSet<CSteamID> players = LoadExistingPlayers();
+        private HashSet<CSteamID> players;
+
+        public KnownPlayers()
+        {
+            players = LoadExistingPlayers();
+        }
 
         private static HashSet<CSteamID> LoadExistingPlayers()
         {
-            string location = (PlayerSavedata.hasSync ? "/Sync/" : UnturnedPaths.RootDirectory.FullName + ServerSavedata.transformPath("/Players/"));
-            HashSet<CSteamID> loaded = new();
-
-            var scan = Directory.GetDirectories(location).Select(Path.GetFileName);
-
-            foreach (var i in scan)
+            HashSet<CSteamID> loaded = [];
+            try
             {
-                if (ulong.TryParse(i.Split("_".ToCharArray())[0], out ulong num))
+                string location = GetPlayersDirectory();
+
+                if (Directory.Exists(location))
                 {
-                    CSteamID playerId = new CSteamID(num);
-                    if (!loaded.Contains(playerId))
+                    var scan = Directory.GetDirectories(location).Select(Path.GetFileName);
+                    foreach (var i in scan)
                     {
-                        loaded.Add(playerId);
+                        if (i != null && i.Contains("_") && ulong.TryParse(i.Split('_')[0], out ulong num))
+                        {
+                            CSteamID playerId = new CSteamID(num);
+                            if (!loaded.Contains(playerId))
+                            {
+                                loaded.Add(playerId);
+                            }
+                        }
+                    }
+                }                
+
+                if (QueueBypasserPlugin.Instance.Configuration.Instance.IsLoggingEnabled)
+                {
+                    if (loaded.Count == 0)
+                    {
+                        Logger.Log("No players have been on the server yet");
+                    } else
+                    {
+                        Logger.Log($"{loaded.Count} players have been on the server");
                     }
                 }
             }
-
-            if (QueueBypasserPlugin.Instance.Configuration.Instance.IsLoggingEnabled)
+            catch (Exception ex)
             {
-                if (loaded.Count == 0)
-                {
-                    Logger.Log($"No players were been on the server yet");
-                }
-                else
-                {
-                    Logger.Log($"{loaded.Count} players were been already on the server");
-                }
+                Logger.LogError($"Error loading known players: {ex.Message}");
             }
 
             return loaded;
+        }
+
+        private static string GetPlayersDirectory()
+        {
+            if (PlayerSavedata.hasSync)
+            {
+                return Path.Combine(UnturnedPaths.RootDirectory.FullName, "Sync");
+            }
+
+            string playersPath = Path.Combine(UnturnedPaths.RootDirectory.FullName, ServerSavedata.transformPath("Players"));
+            return playersPath;
         }
 
         public bool Add(CSteamID playerId)
